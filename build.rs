ce482833ee20ff7fb8800b6f8b25176c8587d0ad
laurent <laurent.mazare@gmail.com>
2023-03-11 18:00:11 +0000

    Do not use the as-needed flag on macos.

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() {
-    println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
-    println!("cargo:rustc-link-arg=-Wl,--copy-dt-needed-entries");
-    println!("cargo:rustc-link-arg=-ltorch");
+    let os = std::env::var("CARGO_CFG_TARGET_OS").expect("Unable to get TARGET_OS");
+    match os.as_str() {
+        "linux" | "windows" => {
+            println!("cargo:rustc-link-arg=-Wl,--no-as-needed");
+            println!("cargo:rustc-link-arg=-Wl,--copy-dt-needed-entries");
+            println!("cargo:rustc-link-arg=-ltorch");
+        }
+        _ => {}
+    }
 }
